7e9a594fdfbc66aea141fdeaee4e6bb438c0fef2
Adil Asharaf <adilshabkv007@gmail.com>
2024-04-21 20:37:43 +0530

    added react webpack and typescript

---
+++ src/react/components/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main className="">
+      <div></div>
+      <button></button>
+    </main>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"], // Adjust paths if needed
+    theme: {
+      extend: {}, // Add your custom theme configurations here
+    },
+    plugins: [], // Add any Tailwind plugins you want to use
+  };
+  
